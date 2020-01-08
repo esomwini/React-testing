27b5f50abfe0fe881707b074012a82ab8894c003
winiesom <esomwini@gmail.com>
2020-01-08 14:00:02 +0100

    Adding PropTypes to our healine component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './component/header';
-import './app.scss';
 import Headline from './component/headline';
+import './app.scss';
 
-function App() {
+const tempArr = [{
+  fName: 'Joe',
+  lName: 'Bloggs',
+  email: 'joebloggs@gmail.com',
+  age: 24,
+  onlineStatus: true
+}]
+
+class App extends Component {
+  render() {
   return (
     <div className="App">
       <Header />
       <section className='main'>
-          <Headline header='Posts' desc='Click the button to render posts' />
+          <Headline header='Posts' desc='Click the button to render posts!' tempArr={tempArr} />
       </section>
     </div>
   );
 }
+};
 
 export default App;
